6411d58a14baea87ef5d37ef95c54b1b9479c0f7
Manah Kallon <manahkallon@dbc-mannahkallon.local>
2016-11-19 16:27:40 -0800

    adds table

---
+++ app/assets/javascripts/components/board.js.jsx
@@ -0,0 +1,38 @@
+var Board = React.createClass({
+  propTypes: {
+    width: React.PropTypes.node,
+    height: React.PropTypes.node
+  },
+
+  render: function() {
+    var table = {
+      "border": "1px solid black",
+      "margin": "0 auto",
+      "border-collapse": "collapse",
+      "background": "black";
+    }
+    var td = {
+      "width": "40px",
+      "height": "40px"
+    }
+
+    var wSquare = {
+
+      "background": "black";
+    }
+  }
+</style>
+
+<table class="bored">
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+</table>
+    );
+  }
+});
